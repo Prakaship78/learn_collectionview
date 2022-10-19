8e3ddc60f4edd1d39dde6b6827c3542751090d37
prakaship78 <prakash.singh@american-technology.net>
2022-10-19 20:34:58 +0530

    Add definiations for basic functions

---
+++ Collectionview/MyCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  MyCollectionViewCell.swift
+//  Collectionview
+//
+//  Created by Prakash on 19/10/22.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView : UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- Collectionview/ViewController.swift
+++ Collectionview/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.delegate = self
+        collectionView.dataSource = self
        
         
         }
@@ -21,16 +22,31 @@ class ViewController: UIViewController {
 
 }
 
+// interaction
 extension ViewController : UICollectionViewDelegate {
-    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+        print("you tapped me ")
+    }
 }
 
 extension ViewController : UICollectionViewDataSource {
     
+    // no of items
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 12
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "", for: indexPath)
+        return cell
+    }
 }
 
-extension ViewController : UICollectionViewDelegateFlowLayout {
-    
-}
+
+// decides margin and padding between each cell
+//extension ViewController : UICollectionViewDelegateFlowLayout {
+//
+//}
 
 
